3a7967aabc3a355150a0a123609e8cf872ce188c
dnjsgml511 <dnjsgml511406@gmail.com>
2023-07-06 00:41:39 +0900

    feat: cors 수정

--- src/main/kotlin/com/ecommerce/api/ecommerce/framework/config/cors/CorsConfig.kt
+++ src/main/kotlin/com/ecommerce/api/ecommerce/framework/config/cors/CorsConfig.kt
@@ -1,6 +1,5 @@
 package com.ecommerce.api.ecommerce.framework.config.cors
 
-import mu.KotlinLogging
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.EnableWebFlux
@@ -10,13 +9,12 @@ import org.springframework.web.reactive.config.WebFluxConfigurer
 @EnableWebFlux
 class CorsConfig : WebFluxConfigurer {
 
-    val log = KotlinLogging.logger {  }
-
     override fun addCorsMappings(registry: CorsRegistry){
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // any host or put domain(s) here
-                .allowedMethods("GET","POST","OPTIONS") // put the http verbs you want allow
-                .allowedHeaders("*") // put the http headers you want allow
-                .allowCredentials(true)
+                .allowedOrigins("*")
+                .allowCredentials(false)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
     }
 }
